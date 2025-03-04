2944bb53d935157e708a4711c6742d77fb8139c8
Joseph Potentier <9673684+JoePotentier@users.noreply.github.com>
2025-03-03 19:51:48 -0700

    Initial Commit

---
+++ go-time/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  go-time
+//
+//  Created by Joseph Potentier on 2025-03-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ go-time/go_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  go_timeApp.swift
+//  go-time
+//
+//  Created by Joseph Potentier on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct go_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
